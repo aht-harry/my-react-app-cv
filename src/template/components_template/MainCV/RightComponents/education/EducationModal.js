df33640536de2d143d99d93524a95f3b1b2b881d
GiangNV <giangnv@arrowhitech.co>
2023-03-31 10:42:37 +0700

    giang

--- src/template/components_template/MainCV/RightComponents/education/EducationModal.js
+++ src/template/components_template/MainCV/RightComponents/education/EducationModal.js
@@ -1,5 +1,10 @@
+import {
+  faCheckCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { Button, Modal, Form } from "react-bootstrap";
+import { Button, Modal, Form, Row, Col } from "react-bootstrap";
 function EducationModal(props) {
   const [companyName, setCompanyName] = useState("");
   const [position, setPosition] = useState("");
@@ -31,59 +36,162 @@ function EducationModal(props) {
   return (
     <Modal show={props.show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Thêm kinh nghiệm làm việc</Modal.Title>
+        <Modal.Title>Thêm mới học vấn bằng cấp</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
-          <Form.Group controlId="companyName">
-            <Form.Label>Tên công ty:</Form.Label>
-            <Form.Control
-              type="text"
-              value={companyName}
-              onChange={(e) => setCompanyName(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="position">
-            <Form.Label>Vị trí làm việc:</Form.Label>
-            <Form.Control
-              type="text"
-              value={position}
-              onChange={(e) => setPosition(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="fromDate">
-            <Form.Label>Từ ngày:</Form.Label>
-            <Form.Control
-              type="date"
-              value={fromDate}
-              onChange={(e) => setFromDate(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="toDate">
-            <Form.Label>Đến ngày:</Form.Label>
-            <Form.Control
-              type="date"
-              value={toDate}
-              onChange={(e) => setToDate(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="description">
-            <Form.Label>Mô tả công việc:</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </Form.Group>
+        <Form className="education_form">
+          <Row className="row-popup">
+            <Col md={6}>
+              <Form.Group controlId="companyName">
+                <Form.Label>
+                  Trường, trung tâm đào tạo:
+                  <span className="required">*</span>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Nhập tên"
+                  required
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="companyName">
+                <Form.Label>Khoa đào tạo: </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Nhập tên khoa"
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="row-popup">
+            <Col md={12}>
+              <Form.Group controlId="companyName">
+                <Form.Label>
+                  Tên bằng cấp chứng chỉ:
+                  <span className="required">*</span>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="VD: Cử nhân luật, Kỹ sư CNTT, Chứng chỉ nghề điện dân dụng..."
+                  required
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="row-popup">
+            <Col md={6}>
+              <Form.Group controlId="companyName">
+                <Form.Label>Chuyên ngành đào tạo:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="VD: Công nghệ thông tin, Kế toán..."
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="companyName">
+                <Form.Label>
+                  Xếp loại: <span className="required">*</span>
+                </Form.Label>
+                <select
+                  className="select-dropdown select-dropdown-nomal valid"
+                  name="skill_software[Ms Word]"
+                  data-placeholder="Chọn"
+                  aria-invalid="false"
+                >
+                  <option class="select-dropdown_item" value="">
+                    Chọn xếp loại
+                  </option>
+                  <option class="select-dropdown_item" value="">
+                    Yếu
+                  </option>
+                  <option class="select-dropdown_item" value="">
+                    Trung bình
+                  </option>
+                  <option class="select-dropdown_item" value="">
+                    Trung bình / Khá
+                  </option>
+                  <option class="select-dropdown_item" value="">
+                    Khá
+                  </option>
+                  <option class="select-dropdown_item" value="">
+                    Giỏi
+                  </option>
+                  <option class="select-dropdown_item" value="">
+                    Xuất sắc
+                  </option>
+                </select>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="row-popup">
+            <Col md={6}>
+              <Form.Group controlId="fromDate">
+                <Form.Label>
+                  Thời gian bắt đầu: <span className="required">*</span>
+                </Form.Label>
+                <Form.Control
+                  type="date"
+                  value={fromDate}
+                  onChange={(e) => setFromDate(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="toDate">
+                <Form.Label>
+                  Thời gian kết thúc: <span className="required">*</span>
+                </Form.Label>
+                <Form.Control
+                  type="date"
+                  value={toDate}
+                  onChange={(e) => setToDate(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="row-popup">
+            <Col md={12}>
+              <Form.Group controlId="companyName">
+                <Form.Label>Mô tả thêm:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
-          Hủy
+          <span>
+            <FontAwesomeIcon
+              icon={faTimesCircle}
+              style={{ marginRight: "5px" }}
+            />
+            Bỏ qua
+          </span>
         </Button>
         <Button variant="primary" onClick={handleSave}>
-          Lưu
+          <span>
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              style={{ marginRight: "5px" }}
+            />
+            Cập nhật
+          </span>
         </Button>
       </Modal.Footer>
     </Modal>
--- src/template/components_template/MainCV/RightComponents/experience/ExperienceModal.js
+++ src/template/components_template/MainCV/RightComponents/experience/ExperienceModal.js
@@ -1,7 +1,9 @@
+import { faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { Button, Modal, Form } from "react-bootstrap";
+import { Button, Modal, Form, Row, Col } from "react-bootstrap";
 function ExperienceModal(props) {
-    const [companyName, setCompanyName] = useState("");
+  const [companyName, setCompanyName] = useState("");
   const [position, setPosition] = useState("");
   const [fromDate, setFromDate] = useState("");
   const [toDate, setToDate] = useState("");
@@ -28,67 +30,94 @@ function ExperienceModal(props) {
     props.onClose();
   };
 
-    return (
-      <Modal show={props.show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Thêm kinh nghiệm làm việc</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="companyName">
-              <Form.Label>Tên công ty:</Form.Label>
-              <Form.Control
-                type="text"
-                value={companyName}
-                onChange={(e) => setCompanyName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="position">
-              <Form.Label>Vị trí làm việc:</Form.Label>
-              <Form.Control
-                type="text"
-                value={position}
-                onChange={(e) => setPosition(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="fromDate">
-              <Form.Label>Từ ngày:</Form.Label>
-              <Form.Control
-                type="date"
-                value={fromDate}
-                onChange={(e) => setFromDate(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="toDate">
-              <Form.Label>Đến ngày:</Form.Label>
-              <Form.Control
-                type="date"
-                value={toDate}
-                onChange={(e) => setToDate(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="description">
-              <Form.Label>Mô tả công việc:</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Hủy
-          </Button>
-          <Button variant="primary" onClick={handleSave}>
-            Lưu
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-  
-  export default ExperienceModal;
-  
+  return (
+    <Modal show={props.show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Thêm kinh nghiệm làm việc</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Row className="row-popup">
+            <Col md={6}>
+              <Form.Group controlId="companyName">
+                <Form.Label>Tên công ty:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="position">
+                <Form.Label>Vị trí làm việc:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={position}
+                  onChange={(e) => setPosition(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="row-popup">
+            <Col md={6}>
+              <Form.Group controlId="fromDate">
+                <Form.Label>Từ ngày:</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={fromDate}
+                  onChange={(e) => setFromDate(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="toDate">
+                <Form.Label>Đến ngày:</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={toDate}
+                  onChange={(e) => setToDate(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="row-popup">
+            <Col md={12}>
+              <Form.Group controlId="description">
+                <Form.Label>Mô tả công việc:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          <span>
+            <FontAwesomeIcon
+              icon={faTimesCircle}
+              style={{ marginRight: "5px" }}
+            />
+            Bỏ qua
+          </span>
+        </Button>
+        <Button variant="primary" onClick={handleSave}>
+          <span>
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              style={{ marginRight: "5px" }}
+            />
+            Cập nhật
+          </span>
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default ExperienceModal;
